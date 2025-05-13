3c830d53df116c9ce5d2c1d64e322cd95a854dc8
Federico <fededeniard@gmail.com>
2025-05-13 10:31:25 -0300

    pupeteer-railway

--- src/scrapping/index.ts
+++ src/scrapping/index.ts
@@ -7,7 +7,7 @@ export const downloadSheet = async (url: string): Promise<{ images: string[], pd
         const page = await browser.newPage();
 
         await page.setViewport({ width: 1920, height: 1080 });
-        await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 30000 });
+        await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 120000 });
 
 
         //Obtain the title
