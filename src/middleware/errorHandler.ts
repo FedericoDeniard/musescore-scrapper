573544f38ac30f8336172ba562a0bb2045a27cdd
Federico <fededeniard@gmail.com>
2025-05-13 01:49:57 -0300

    console.log errors

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,5 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpError, ResponseObject } from 'src/utils/response';
 
 export const errorHandler = (err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    console.log(err.message)
     res.status(err.status || 500).json(new ResponseObject(false, err.message, null));
 }
